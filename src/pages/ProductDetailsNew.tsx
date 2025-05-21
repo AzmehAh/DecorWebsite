d3a2449806cf764c6ac929e86703c432604ba826
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-05-21 16:24:54 +0300

    Deploy application to Netlify

--- src/pages/ProductDetailsNew.tsx
+++ src/pages/ProductDetailsNew.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
 import { motion, AnimatePresence } from "framer-motion";
+import ReactMarkdown from 'react-markdown';
 import {
   ChevronLeft,
   ChevronDown,
@@ -600,7 +601,7 @@ export default function ProductDetailsNew() {
                           </h4>
                           <div className="text-gray-600 space-y-1">
                          {product.surface_preparation.split("\n").map((line, index) => (
-                        <p key={index}>{line}</p>
+                        <ReactMarkdown className="prose"> key={index}>{line}</ReactMarkdown>
                               ))}
                                </div>
                             </div>
