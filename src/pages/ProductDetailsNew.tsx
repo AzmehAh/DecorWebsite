f2b34f0e57cd46dfeadbfd76bf830a7727bba9d1
AzmehAh <ahmad.m.azmeh@gmail.com>
2025-05-21 14:23:42 +0300

    Updated ProductDetailsNew.tsx

--- src/pages/ProductDetailsNew.tsx
+++ src/pages/ProductDetailsNew.tsx
@@ -619,7 +619,7 @@ export default function ProductDetailsNew() {
                           {product.dry_to_touch && (
                             <div>
                               <h4 className="font-medium mb-3">
-                                {t("productDetails.dryingTime.title")}
+                                {t("productDetails.dryingTime.title")}:
                               </h4>
                               <div className="grid md:grid-cols-2 gap-4 mb-1">
                                 <div>
